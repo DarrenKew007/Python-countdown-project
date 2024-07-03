2c65a7b1f7a7314143d99bb50ca9cfb87e5dd7cf
Darren Kew <149661299+DarrenKew007@users.noreply.github.com>
2024-07-04 03:52:33 +0800

    Update Countdown project.py

--- Countdown project.py
+++ Countdown project.py
@@ -1,17 +1,19 @@
-import time 
-  
-def countdown(t): 
-    
-    while t: 
-        mins, secs = divmod(t, 60) 
-        timer = '{:02d}:{:02d}'.format(mins, secs) 
-        print(timer, end="\r") 
-        time.sleep(1) 
-        t -= 1
-      
-    print("It's Showtime!") 
-  
+import time
 
-t = input("Please enter the time to countdown in seconds: ") 
-  
-countdown(int(t)) 
+def countdown_timer(seconds):
+    for t in range(seconds, 0, -1):
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+    
+    print("It's Showtime!")
+
+# Get user input for the countdown time
+user_input = input("Please enter the time to countdown in seconds: ")
+
+try:
+    countdown_time = int(user_input)
+    countdown_timer(countdown_time)
+except ValueError:
+    print("Invalid input. Please enter a valid integer for the countdown time.")
